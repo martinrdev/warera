7e236e0a0b44ac51aa97fddca5232341db3e5d1d
martinrdev <martinreyes32@outlook.com>
2025-05-31 01:34:31 -0300

    fetching data and storing to db

--- src/market-history-collector-job/src/index.ts
+++ src/market-history-collector-job/src/index.ts
@@ -15,6 +15,12 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { TradingPricesResult } from "./response";
+
+export interface Env {
+	DB: D1Database;
+}
+
 export default {
 	async fetch(req) {
 		const url = new URL(req.url);
@@ -25,13 +31,26 @@ export default {
 
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
-	async scheduled(event, env, ctx): Promise<void> {
+	async scheduled(event, env: Env, ctx): Promise<void> {
 		// A Cron Trigger can make requests to other endpoints on the Internet,
 		// publish to a Queue, query a D1 Database, and much more.
 		//
 		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		let resp = await fetch('https://api2.warera.io/trpc/itemTrading.getPrices');
+		let wasSuccessful;
+
+		if (resp.ok) {
+			wasSuccessful = 'success';
+
+			const result = await resp.json() as TradingPricesResult;
+			const timestamp = Date.now();
+
+			const ps = env.DB.prepare(`INSERT INTO marketHistory (product, timestamp, price) VALUES (?, ?, ?)`);
+			const queries = Object.entries(result.result.data).map(([product, price]) => ps.bind(product, timestamp, price));
+			await env.DB.batch(queries);
+		} else {
+			wasSuccessful = 'fail';
+		}
 
 		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
 		// In this template, we'll just log the result:
