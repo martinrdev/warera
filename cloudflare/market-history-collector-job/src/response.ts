e041b2900b2f0401c6960fd6e60d301ab2254624
martinrdev <martinreyes32@outlook.com>
2025-05-31 02:21:00 -0300

    renamed cloudflare source folder

---
+++ cloudflare/market-history-collector-job/src/response.ts
@@ -0,0 +1,24 @@
+export interface TradingPricesResult {
+    result: {
+        data: TradingPrices;
+    };
+}
+
+export interface TradingPrices {
+    steak: number;
+    livestock: number;
+    coca: number;
+    cocain: number;
+    cookedFish: number;
+    heavyAmmo: number;
+    concrete: number;
+    fish: number;
+    bread: number;
+    ammo: number;
+    limestone: number;
+    grain: number;
+    iron: number;
+    steel: number;
+    lead: number;
+    case1: number;
+}
