f7708f07184fb8824ec658390be1704e96ed4630
martinrdev <martinreyes32@outlook.com>
2025-06-01 00:56:58 -0300

    showing all

--- streamlit/market_history/main.py
+++ streamlit/market_history/main.py
@@ -16,11 +16,12 @@ headers = {
 
 @st.fragment()
 def render_market_history():
+    st.header("Market Prices")
+
     body = {
         "query": "SELECT * FROM marketHistory",
         "params": [],
     }
-
     result = requests.post(query_url, headers=headers, json=body)
 
     if not result.ok:
@@ -36,25 +37,32 @@ def render_market_history():
         st.stop()
 
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms', utc=True)
-    df.set_index('timestamp', inplace=True)
+    df.set_index('timestamp')
+    
+    latest_timestamp = df['timestamp'].max()
+    st.write(f"Latest data timestamp: {latest_timestamp}")
 
-    st.write("This is the market history data fetched from the API.")
+    latest_df = df[df['timestamp'] == latest_timestamp]
+    latest_df = latest_df.drop(columns=['timestamp'])
+    column_config = {
+        "product": st.column_config.TextColumn("Product"),
+        "price": st.column_config.NumberColumn("Market price", format="%.3f BTC"),
+    }
+    st.dataframe(latest_df, hide_index=True, use_container_width=True, column_config=column_config)
 
     # Pivot the DataFrame so each product is a column, indexed by timestamp
-    df_pivot = df.pivot(columns='product', values='price')
-
-    st.dataframe(df_pivot)
-
+    df_pivot = df.pivot(index='timestamp', columns='product', values='price')
     st.line_chart(df_pivot, use_container_width=True)
 
 
 @st.fragment()
 def render_profit_history():
+    st.header("Work Unit Profit")
+
     body = {
             "query": "SELECT * FROM profitHistory",
             "params": [],
         }
-
     result = requests.post(query_url, headers=headers, json=body)
 
     if not result.ok:
@@ -70,19 +78,25 @@ def render_profit_history():
         st.stop()
 
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms', utc=True)
-    df.set_index('timestamp', inplace=True)
+    df.set_index('timestamp')
+    
+    latest_timestamp = df['timestamp'].max()
+    st.write(f"Latest data timestamp: {latest_timestamp}")
 
-    st.write("This is the profit history data fetched from the API.")
-
+    latest_df = df[df['timestamp'] == latest_timestamp]
+    latest_df = latest_df.drop(columns=['timestamp'])
+    latest_df.sort_values(by='workUnitProfit', inplace=True, ascending=False)
+    column_config = {
+        "product": st.column_config.TextColumn("Product"),
+        "workUnitProfit": st.column_config.NumberColumn("Work Unit Profit", format="%.3f BTC"),
+    }
+    st.dataframe(latest_df, hide_index=True, use_container_width=True, column_config=column_config)
+    
     # Pivot the DataFrame so each product is a column, indexed by timestamp
-    df_pivot = df.pivot(columns='product', values='workUnitProfit')
-    st.dataframe(df_pivot)
-
-    if 'product' in df.columns and 'workUnitProfit' in df.columns:
-        st.line_chart(df_pivot, use_container_width=True)
-    else:
-        st.line_chart(df, use_container_width=True)
+    df_pivot = df.pivot(index='timestamp', columns='product', values='workUnitProfit')
+    st.line_chart(df_pivot, use_container_width=True)
 
 
 render_market_history()
+st.divider()
 render_profit_history()
